565e221615c57d9fe5b563d745a6f5ac4a703f20
Danilshik <danilshik@yandex.ru>
2017-10-06 18:05:47 +0500

    Полнисью выполнены примеры с 4 раздела

--- Лабораторная 4/js/coloring_cube.js
+++ Лабораторная 4/js/coloring_cube.js
@@ -2,7 +2,8 @@ var gl;
 var shaderProgram;
 var vertexBuffer; // буфер вершин
 var indexBuffer; //буфер индексов
-   
+var colorBuffer; //буфер цветов
+ 
 var mvMatrix = mat4.create(); 
 var pMatrix = mat4.create();
 // установка шейдеров
@@ -25,7 +26,9 @@ function initShaders() {
  
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-    // создания переменных uniform для передачи матриц в шейдер
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+     
     shaderProgram.MVMatrix = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     shaderProgram.ProjMatrix = gl.getUniformLocation(shaderProgram, "uPMatrix");
 }
@@ -36,10 +39,12 @@ function setMatrixUniforms(){
 } 
 // Функция создания шейдера
 function getShader(type,id) {
-     
     var source = document.getElementById(id).innerHTML;
+ 
     var shader = gl.createShader(type);
+     
     gl.shaderSource(shader, source);
+ 
     gl.compileShader(shader);
    
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -62,15 +67,33 @@ function initBuffers() {
                 -0.5, -0.5, -0.5,
                 -0.5, 0.5, -0.5,
                  0.5, 0.5, -0.5,
-                 0.5, -0.5, -0.5];
+                 0.5, -0.5, -0.5
+                 ];
                   
-    var indices = [0, 1, 1, 2, 2, 3, 3, 0, 0, 4, 4, 5, 5, 6, 6,7, 7,4, 1, 5, 2, 6, 3, 7];
+    var indices = [ // лицевая часть
+                0, 1, 2, 
+                2, 3, 0,
+                //нижняя часть
+                0, 4, 7, 
+                7, 3, 0,
+                // левая боковая часть
+                0, 1, 5,
+                5, 4, 0, 
+                // правая боковая часть
+                2, 3, 7, 
+                7, 6, 2,
+                // верхняя часть
+                2, 1, 6,
+                6, 5, 1,
+                // задняя часть
+                4, 5, 6,
+                6, 7, 4,
+            ];
      
  // установка буфера вершин
   vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
- 
   vertexBuffer.itemSize = 3;
  
   // создание буфера индексов
@@ -79,25 +102,48 @@ function initBuffers() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
     // указываем число индексов это число равно числу индексов
     indexBuffer.numberOfItems = indices.length;
+ 
+    // установка цветов для каждой вершины
+    var сolors = [
+        0.0, 0.0, 0.3,
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.3, 0.0,
+         
+        0.0, 0.0, 0.3,
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.3, 0.0
+    ];
+    colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(сolors), gl.STATIC_DRAW);
 }
   
 function draw() {    
-     
+ 
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
                          vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
  
-    gl.drawElements(gl.LINES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
+                        vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+     
+    gl.enable(gl.DEPTH_TEST);
+     
+    gl.drawElements(gl.TRIANGLES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
 }
 function setupWebGL()
 {
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);  
-    gl.clear(gl.COLOR_BUFFER_BIT);  
+    gl.clearColor(0.9, 1.0, 1.0, 1.0);  
+    gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);   
                  
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     mat4.perspective(pMatrix, 1.04, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix,mvMatrix,[0, 0, -2.0]);
-    mat4.rotate(mvMatrix,mvMatrix, 1.9, [0, 1, 0]); 
+    mat4.rotate(mvMatrix,mvMatrix, 2.0, [0, 1, 0]); 
 }
   
 window.onload=function(){
@@ -114,11 +160,11 @@ window.onload=function(){
     if(gl){
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
+ 
         initShaders();
-         
         initBuffers();
         setupWebGL();
         setMatrixUniforms();
-        draw();  
+        draw(); 
     }
 }
