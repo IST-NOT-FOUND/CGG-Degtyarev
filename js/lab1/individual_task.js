80ce71629aeb646b44ff5e4df988401f212f6c61
Danilshik <danilshik@yandex.ru>
2017-10-20 15:11:56 +0500

    Реализован ромб для индивидуального задания

--- js/lab1/individual_task.js
+++ js/lab1/individual_task.js
@@ -1,37 +1,36 @@
 var gl;
 var shaderProgram;
-var vertexBuffer;
+var vertexBuffer; // буфер вершин
+var indexBuffer; //буфер индексов
 // установка шейдеров
 function initShaders() {
-    // получаем шейдеры
     var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
     var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
-    //создаем объект программы шейдеров
+
     shaderProgram = gl.createProgram();
-    // прикрепляем к ней шейдеры
+
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
-    // связываем программу с контекстом webgl
+
     gl.linkProgram(shaderProgram);
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Не удалсь установить шейдеры");
+        alert("Не удалось установить шейдеры");
     }
 
     gl.useProgram(shaderProgram);
-    // установка атрибута программы
+
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    // делаем доступным атрибут для использования
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 }
-// Функция создания шейдера по типу и id источника в структуре DOM
+// Функция создания шейдера
 function getShader(type, id) {
     var source = document.getElementById(id).innerHTML;
-    // создаем шейдер по типу
+
     var shader = gl.createShader(type);
-    // установка источника шейдера
+
     gl.shaderSource(shader, source);
-    // компилируем шейдер
+
     gl.compileShader(shader);
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -41,61 +40,78 @@ function getShader(type, id) {
     }
     return shader;
 }
-// установка буфера вершин 
+// установка буферов вершин и индексов
 function initBuffers() {
+
+    vertices = [-0.7, -0.4, 0.0,
+				-0.2, 0.4, 0.0,
+				0.2, -0.4, 0.0,
+				0.7, 0.4, 0.0,
+			];
+
+    indices = [0, 1, 2,1,2,3];
     // установка буфера вершин
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // массив координат вершин объекта
-    var triangleVertices = [
-        0.0, 0.5, 0.0,
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-    // указываем кол-во точек
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // размерность
     vertexBuffer.itemSize = 3;
-    vertexBuffer.numberOfItems = 3;
+
+    // создание буфера индексов
+    indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    // указываем число индексов это число равно числу индексов
+    indexBuffer.numberOfItems = indices.length;
 }
-// отрисовка 
+// отрисовка
 function draw() {
-    // установка области отрисовки
+
+    gl.clearColor(1.0, 0.0, 1.0, 1.0); //внешний
+
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // указываем, что каждая вершина имеет по три координаты (x, y, z)
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
             vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    // отрисовка примитивов - треугольников          
-    gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.numberOfItems);
+    // отрисовка треугольников
+    gl.drawElements(gl.TRIANGLES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT, 0);
 }
 
 window.onload = function () {
-    // получаем элемент canvas
+
     var canvas = document.getElementById("canvas3D");
     try {
-        // Сначала пытаемся получить стандартный контекст WegGL
-        // Если не получится, обращаемся к экспериментальному контексту
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
     } catch (e) {
     }
 
-    // Если контекст не удалось получить, выводим сообщение
     if (!gl) {
         alert("Ваш браузер не поддерживает WebGL");
     }
     if (gl) {
-        // установка размеров области рисования
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
-        // установка шейдеров 
+
         initShaders();
-        // установка буфера вершин
         initBuffers();
-        // покрасим фон в бледно-розовый цвет
-        gl.clearColor(0, 0, 1, 1.0);
-        // отрисовка сцены
         draw();
     }
+}
+function changeBGColor(){
+	gl.clearColor(getRandomValue(), getRandomValue(), getRandomValue(), 1.0);
+
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+            vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    // отрисовка треугольников
+    gl.drawElements(gl.TRIANGLES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT, 0);
+	
+}
+function getRandomValue() {
+    return Math.random();
 }
