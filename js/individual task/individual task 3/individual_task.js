718efa7a1eaf1b862a079cd070d1cf0b5f7bc95e
Danilshik <danilshik@yandex.ru>
2017-12-09 21:16:11 +0500

    Добавлены переменные для временного хранения цветов и для постоянного, Изменена табуляция некоторых строчек Изменен цвет подложки на черный, чтобы более лучше рассмотреть треугольник Добавлена функция изменения цветов Добавлена функция замены цветов Добавлена функция инициализации для слайдера

--- js/individual task/individual task 3/individual_task.js
+++ js/individual task/individual task 3/individual_task.js
@@ -2,6 +2,21 @@ var gl;
 var shaderProgram;
 var vertexBuffer; // буфер вершин
 var colorBuffer; //буфер цветов
+var sliderValue;
+
+var v1r=0.0, v1g=0.5, v1b=1,
+    v2r=1.0, v2g=1.0, v2b=0,
+    v3r=0.8, v3g=0.0, v3b=0,
+    v1r_temp=v1r,
+    v1g_temp=v1g,
+    v1b_temp=v1b,
+    v2r_temp=v2r,
+    v2g_temp=v2g,
+    v2b_temp=v2b,
+    v3r_temp=v3r,
+    vr3g_temp=v3g,
+    vr3b_temp=v3b;
+
 // установка шейдеров
 function initShaders() {
     var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
@@ -27,14 +42,10 @@ function initShaders() {
 }
 // Функция создания шейдера
 function getShader(type,id) {
-    var source = document.getElementById(id).innerHTML;
- 
-    var shader = gl.createShader(type);
-     
-    gl.shaderSource(shader, source);
- 
-    gl.compileShader(shader);
-   
+    var source = document.getElementById(id).innerHTML; 
+    var shader = gl.createShader(type);     
+    gl.shaderSource(shader, source); 
+    gl.compileShader(shader);   
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         alert("Ошибка компиляции шейдера: " + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);   
@@ -43,23 +54,23 @@ function getShader(type,id) {
     return shader;  
 }
  // установка буферов вершин и индексов
-function initBuffers() {
- 
-var vertices = [
+function initBuffers() { 
+    var vertices = [
          0.0,  0.5,  0.0,
         -0.5, -0.5,  0.0,
          0.5, -0.5,  0.0
-  ];
+    ];
   // установка буфера вершин
-  vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  vertexBuffer.itemSize = 3;
-  vertexBuffer.numberOfItems = 3;
-  var сolors = [
-        0.0, 0.0, 0.0,
-        1.0, 1.0, 1.0,
-        0.0, 1.0, 0.0
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = 3;
+    vertexBuffer.numberOfItems = 3;
+
+    var сolors = [
+        v1r, v1g, v1b,
+        v2r, v2g, v2b,
+        v3r, v3g, v3b        
     ];
     colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
@@ -68,7 +79,7 @@ var vertices = [
  // отрисовка
 function draw() {    
      
-    gl.clearColor(1.0, 1.0, 0.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT);
      
@@ -96,12 +107,50 @@ window.onload=function(){
       }
     if(gl){
         gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
-         
-        initShaders();
- 
-        initBuffers();
- 
-        draw();     
+        gl.viewportHeight = canvas.height;         
+        initShaders(); 
+        initBuffers(); 
+        draw();
+        
     }
 }
+
+function changeColor(color,colorNeighbor){
+    var difference=(color-colorNeighbor)/100
+    return color-difference*sliderValue;
+}
+function replacementColor(){
+    sliderValue=document.getElementById("slider").value;
+    temp1r=v1r;
+    temp1g=v1g;
+    temp1b=v1b;
+
+    v1r_temp=changeColor(v1r, v3r);
+    v1g_temp=changeColor(v1g, v3g)
+    v1b_temp=changeColor(v1b, v3b);
+    v3r_temp=changeColor(v3r,v2r);
+    v3g_temp=changeColor(v3g,v2g);
+    v3b_temp=changeColor(v3b,v2b);
+    v2r_temp=changeColor(v2r, temp1r);
+    v2g_temp=changeColor(v2g, temp1g);
+    v2b_temp=changeColor(v2b, temp1b);    
+
+    var сolors = [
+       v1r_temp, v1g_temp, v1b_temp,
+       v2r_temp, v2g_temp, v2b_temp,
+       v3r_temp, v3g_temp, v3b_temp
+   ];
+   colorBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(сolors), gl.STATIC_DRAW);
+}
+function initSlider(){    
+    initShaders();
+    replacementColor();
+    draw();
+}
+
+
+
+        
+        
