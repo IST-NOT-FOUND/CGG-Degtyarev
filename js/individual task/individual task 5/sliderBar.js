8062b8779399c6dba1620d542aec3d96876bdb42
DESKTOP-EDLDO2A\Danilshik <danilshik@yandex.ru>
2017-12-22 19:52:49 +0500

    Реализована функция изменения значений слайдера

--- js/individual task/individual task 5/sliderBar.js
+++ js/individual task/individual task 5/sliderBar.js
@@ -1,1 +1,22 @@
+// With JQuery
+$("#sliderbarHorizontal").slider({
+	min: -1,
+	max: 1,
+	value: 0,
+	tooltip_position:'bottom'
+});
+$("#sliderbarVertical").slider({
+	min: -1,
+	max: 1,
+	value: 0,	
+	tooltip_position:'bottom'
+	
+});
+
+$("#sliderbarHorizontal").on("slide", function(slideEvt) {
+	$("#sliderbarHorizontalValue").text(slideEvt.value);
+});
+$("#sliderbarVertical").on("slide", function(slideEvt) {
+	$("#sliderbarVerticalValue").text(slideEvt.value);
+});
 
